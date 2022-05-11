55684857604acb285cafd339b9b34313db9965d4
Hiwot Taye <you@example.com>
2022-05-11 17:56:15 -0400

    Completed our final project with a team of 5

---
+++ js/taskmanager.js
@@ -0,0 +1,124 @@
+const createTaskHtml = (id, name, description, assignedTo, dueDate, status) => `
+  <li class="list-group-item" data-task-id=${id}>
+        <div id="d-flex w-100 mt-2 justify-content-between align-items-center" class=form>
+            <h5>${name}</h5>
+            <span class="badge ${status === 'TODO' ? 'badge-secondary' : 'badge-success'}">${status}</span>
+            </div>
+          <div class="d-flex w-100 mt-2 justify-content-between">
+                <small>Assigned To:${assignedTo}</small>
+                <small>Due: ${dueDate}</small>
+          </div>
+          <p id=descriptionCard>${description}</p>
+          <div class="d-flex w-100 justify-content-end">
+            <button id=doneCard class="btn btn-outline-success done-button ${status === 'TODO' ? 'visible' : 'invisible'}">
+              Mark As Done
+            </button>
+            <button id=deleteCard class="btn btn-secondary delete-button">Delete</button>
+          </div>
+  </li>
+  <br>`;
+                                                          
+
+class TaskManager {
+  constructor(currentId = 0){
+    this.tasks = [];
+    this.currentId = currentId;
+  } 
+
+  addTask(name, description, assignedTo, dueDate) {
+     const task = {
+      id: this.currentId++,
+      name: name,
+      description: description,
+      assignedTo: assignedTo,
+      dueDate: dueDate,  
+      status: 'TODO'
+    };
+  
+    this.tasks.push(task);
+  }
+
+  deleteTask(taskId) {
+    
+    const newTasks = [];
+
+    for (let i = 0; i < this.tasks.length; i++) {
+      
+      const task = this.tasks[i];
+
+      if (task.id !== taskId) {
+
+        newTasks.push(task);
+      }   
+    }
+    
+    this.tasks = newTasks;
+  }
+
+  getTaskById(taskId) {
+      let foundTask;
+  
+      for (let i = 0; i < this.tasks.length; i++) {
+        const task = this.tasks[i];
+  
+        if (task.id === taskId) {
+          foundTask = task;
+        }
+      }
+  
+      return foundTask;
+    }
+  
+  render() {
+    const tasksHtmlList = [];
+
+    for(let i = 0; i < this.tasks.length; i++) {
+      const task = this.tasks[i];
+
+      const date = new Date(task.dueDate);
+      const formattedDate = (date.getMonth() + 1) + '/' + (date.getDate() + 1) + '/' + date.getFullYear();
+
+      const taskHtml = createTaskHtml(task.id, task.name, task.description, task.assignedTo, formattedDate, task.status);
+
+      tasksHtmlList.push(taskHtml);
+    }
+    
+    const tasksHtml = tasksHtmlList.join('\n');
+
+    const tasksList = document.querySelector('#tasksList');
+    tasksList.innerHTML = tasksHtml;
+  } 
+  
+  save() {
+
+    const tasksJson = JSON.stringify(this.tasks);
+
+    localStorage.setItem('tasks', tasksJson);
+
+    const currentId = String(this.currentId);
+
+    localStorage.setItem('currentId', currentId);
+  }
+
+  load() {
+
+    if (localStorage.getItem('tasks')) {
+      
+      const tasksJson = localStorage.getItem('tasks');
+
+      this.tasks = JSON.parse(tasksJson);
+    }
+    
+    if (localStorage.getItem('currentId')) {
+      
+      const currentId = localStorage.getItem('currentId');
+
+      this.currentId = Number(currentId);
+    }
+  }
+}
+
+
+  
+
+//final product
